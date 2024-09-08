0f51a7cce11eecbf9944c85f719e85b6f87bdd0f
rahulan <srirahulan1995@gmail.com>
2024-09-08 12:01:39 +0530

    Updated the proper gulp version

--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -43,7 +43,7 @@ class WebContainerCest
     }
 
     public function checkPHPUnit8Version(UnitTester $I){
-        $I->wantTo("verify phpunit 8 library is installed in the container");
+        $I->wantTo("verify phpunit 8 library is installed in the scontainer");
         $I->runShellCommand("docker exec test_web phpunit8 --version");
         $I->seeInShellOutput('PHPUnit 8.2.5');
     }
@@ -51,14 +51,14 @@ class WebContainerCest
     public function checkNcCommand(UnitTester $I){
         $I->wantTo("verify nc command is installed in the image");
         $I->runShellCommand("docker exec test_web nc -h");
-        $I->seeInShellOutput('netcat');
+        $I->seeInShellOutput('OpenBSD netcat (Debian patchlevel 1.206-1ubuntu1)');
 
     }
 
     public function checkXdebugVersion(AcceptanceTester $I){
         $I->wantTo("verify xdebug is installed in the image");
         $I->runShellCommand("docker exec test_web bash -c 'php --version | grep Xdebug'");
-        $I->seeInShellOutput('Version');
+        $I->seeInShellOutput('Xdebug');
         $I->seeInShellOutput('v3.1.4');
     }
 
@@ -78,8 +78,8 @@ class WebContainerCest
 
     public function checkNodeIsInstalled(AcceptanceTester $I){
         $I->wantTo("verify node is installed in the image");
-        $I->runShellCommand('docker exec test_web export NVM_DIR="/usr/local/nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";node -v');
-        $I->seeInShellOutput('v6');
+        $I->runShellCommand('docker exec test_web bash -c \'export NVM_DIR="/usr/local/nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";node -v;\'');
+        $I->seeInShellOutput('v6.17.1');
     }
 
 
@@ -97,7 +97,7 @@ class WebContainerCest
 
     public function checkBowerIsInstalled(AcceptanceTester $I){
         $I->wantTo("verify bower is installed in the image");
-        $I->runShellCommand("docker exec test_web bower --version");
+        $I->runShellCommand('docker exec test_web bash -c \'export NVM_DIR="/usr/local/nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";bower --version;\'');
         $I->seeInShellOutput('1.8');
     }
 
