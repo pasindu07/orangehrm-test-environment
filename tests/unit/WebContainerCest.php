9ea207da4642222be8686cd1542f4816acb62823
deeghayu <deeghayu@orangehrm.com>
2025-01-09 22:04:28 +0530

    updated the phpunit versions

--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -26,25 +26,25 @@ class WebContainerCest
 
     public function checkPHPUnitVersion(UnitTester $I){
         $I->wantTo("verify phpunit 8 library is installed in the container");
-        $I->runShellCommand("docker exec test_web_rhel phpunit --version");
+        $I->runShellCommand("docker exec test_web_rhel phpunit8 --version");
         $I->seeInShellOutput('PHPUnit 8.5.41');
     }
 
     public function checkPHPUnit3Version(UnitTester $I){
         $I->wantTo("verify phpunit 9 library is installed in the container");
-        $I->runShellCommand("docker exec test_web_rhel phpunit3 --version");
+        $I->runShellCommand("docker exec test_web_rhel phpunit9 --version");
         $I->seeInShellOutput('PHPUnit 9.6.22');
     }
 
     public function checkPHPUnit7Version(UnitTester $I){
         $I->wantTo("verify phpunit 10 library is installed in the container");
-        $I->runShellCommand("docker exec test_web_rhel phpunit7 --version");
+        $I->runShellCommand("docker exec test_web_rhel phpunit10 --version");
         $I->seeInShellOutput('PHPUnit 10.5.39');
     }
 
     public function checkPHPUnit8Version(UnitTester $I){
         $I->wantTo("verify phpunit 11 library is installed in the container");
-        $I->runShellCommand("docker exec test_web_rhel phpunit8 --version");
+        $I->runShellCommand("docker exec test_web_rhel phpunit11 --version");
         $I->seeInShellOutput('PHPUnit 11.5.1');
     }
 
