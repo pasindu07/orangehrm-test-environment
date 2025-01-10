9a209052b278cfd9643727b68bd7a83fee94cc53
deeghayu <deeghayu@orangehrm.com>
2025-01-10 11:26:36 +0530

    Updated the composer path

--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -127,7 +127,7 @@ class WebContainerCest
 
     public function checkComposer2IsInstalled(AcceptanceTester $I){
         $I->wantTo("verify composer2 is installed in the image");
-        $I->runShellCommand("docker exec test_web_rhel /user/local/bin/composer --version");
+        $I->runShellCommand("docker exec test_web_rhel /usr/local/bin/composer --version");
         $I->seeInShellOutput('Composer version 2');
     }
 
